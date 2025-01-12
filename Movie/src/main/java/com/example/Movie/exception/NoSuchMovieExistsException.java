6ed3c0329a8e6e564102ee71b92e4cf2fe5871d7
RajeshRathinam <rajeshrasu@gmail.com>
2025-01-12 10:38:53 +0530

    Delete Movie target directory

--- Movie/src/main/java/com/example/Movie/exception/NoSuchMovieExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Movie.exception;
-
-public class NoSuchMovieExistsException extends RuntimeException{
-
-	public NoSuchMovieExistsException(String message) {
-		super(message);
-	}
-}
